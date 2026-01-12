c12467dee1cc0ec77b7664818509dab3f1e799ab
kuruteiru <martin.krutil@email.cz>
2026-01-12 03:44:26 +0100

    movie search tab

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { MovieService, Movie } from '../services/movie.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [CommonModule, IonicModule, FormsModule],
 })
 export class Tab1Page {
-  constructor() {}
+  searchQuery: string = "";
+  imageBaseUrl: string = "https://image.tmdb.org/t/p/w500";
+  movies: Movie[] = [];
+
+  constructor(private movieService: MovieService) { }
+
+  search() {
+    if (this.searchQuery.length > 2) {
+      this.movieService.search(this.searchQuery).subscribe((result) => {
+        this.movieService = result.results;
+      });
+    }
+  }
+
+  addToFavorites(movie: Movie) {
+    this.movieService.addToFavorites(movie);
+    console.log("movie (", movie, ") added to favorites");
+  }
 }
