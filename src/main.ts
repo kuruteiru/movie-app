2456a537e1a06d526773fa433321844b973fa689
kuruteiru <martin.krutil@email.cz>
2026-01-12 03:04:29 +0100

    importing http and storage modules

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+// imports for http requests and on-device storage
+import { provideHttpClient } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage-angular'
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
+    importProvidersFrom(IonicStorageModule.forRoot())
   ],
 });
