c933c952555fa7b5ad2441ea9a3bb04f9db52ed0
kuruteiru <martin.krutil@email.cz>
2026-01-12 09:15:19 +0100

    android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.movie.app',
   appName: 'movie-app',
   webDir: 'www'
 };
