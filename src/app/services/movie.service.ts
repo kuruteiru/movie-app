23017b23e8e999a555ac9903ca328616012311ae
kuruteiru <martin.krutil@email.cz>
2026-01-12 03:43:05 +0100

    movie service

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  private apiKey: string = "key";
+  private baseUrl: string = "https://api.themoviedb.org/3";
+  private storage: Storage | null = null;
+  private FAVORITES_KEY = "favorites";
+
+  constructor(private http: HttpClient, private _storage: Storage) {
+    this.init();
+  }
+
+  private async init() {
+    await this.getStorage();
+  }
+
+  private async getStorage(): Promise<Storage> {
+    if (!this.storage) {
+      this.storage = await this._storage.create();
+    }
+    return this.storage;
+  }
+
+  search(query: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${query}`);
+  }
+
+  async addToFavorites(movie: Movie): Promise<Movie[]> {
+    const storage = await this.getStorage();
+    const current = await storage.get(this.FAVORITES_KEY) || [];
+    current.push(movie);
+    return await this.storage?.set(this.FAVORITES_KEY, current);
+  }
+
+  async getFavorites(): Promise<Movie[]> {
+    const storage = await this.getStorage();
+    return await storage.get(this.FAVORITES_KEY) || [];
+  }
+
+  async removeFromFavorites(movieToRemove: Movie): Promise<Movie[]> {
+    let current = await this.storage?.get(this.FAVORITES_KEY) || [];
+    current = current.filter((m: Movie) => m.id !== movieToRemove.id);
+    return await this.storage?.set(this.FAVORITES_KEY, current);
+  }
+}
+
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  vote_average: number;
+  poster_path: string;
+}
