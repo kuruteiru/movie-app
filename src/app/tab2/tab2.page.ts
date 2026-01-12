2ae7f14b8481d211aadc15d37d7dc9cc3b00a7ee
kuruteiru <martin.krutil@email.cz>
2026-01-12 07:58:30 +0100

    tab2 impl

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { MovieService, Movie } from '../services/movie.service';
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonicModule, CommonModule]
 })
 export class Tab2Page {
+  favoriteMovies: Movie[] = [];
+  imageBaseUrl: string = "https://image.tmdb.org/t/p/w500";
 
-  constructor() {}
+  constructor(private movieService: MovieService) {
+    addIcons({ heart });
+  }
 
+  // runs every time tab2 is opened
+  async ionViewWillEnter() {
+    await this.loadFavorites();
+  }
+
+  async loadFavorites() {
+    this.favoriteMovies = await this.movieService.getFavorites();
+  }
+
+  async removeFavorite(movie: Movie) {
+    await this.movieService.removeFromFavorites(movie);
+    await this.loadFavorites();
+  }
 }
